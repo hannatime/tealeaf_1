0220d51aa5658a461d2668f2f1a6b5d7132f20ea
Ben Hanna <ben.hanna@me.com>
2014-05-01 16:30:47 +1000

    phew, a few hours into this but I'm done!

--- blackjack_2.rb
+++ blackjack_2.rb
@@ -1,10 +1,11 @@
 #Interactive commandline blackjack game
 
+#Say message method
 def say(msg)
   puts "#{msg}"
 end
 
-#[['H', '2'],['D','A'],[...]] = TOTAL
+#Calculate cards method
 def calculate_total(cards) 
   arr = cards.map{|e| e[1] }
 
@@ -27,19 +28,19 @@ def calculate_total(cards)
   total
 end
 
-say "welcome to blackjack"
-say "You have $200, how much would you like to bet?"
-bet = gets.chomp.to_i 
-while true
-  if bet > 0 
-    say "You've bet $#{bet}, lets play!"
-  break
-  else 
-    puts "Don't be stingy"
-    say "You have $200, how much would you like to bet?"
-    bet = gets.chomp.to_i
-  end
-end
+#Game Start
+say "-----------------------------------------"
+say "-----------------------------------------"
+say "Welcome to Blackjack"
+say "-----------------------------------------"
+say "-----------------------------------------"
+say "What should I call you?"
+player_name = gets.chomp
+say "-----------------------------------------"
+say "-----------------------------------------"
+say ""
+say "Hi #{player_name.capitalize}, lets play"
+say ""
 
 suits = ['H', 'D', 'S', 'C']
 cards = ['2', '3', '4', '5', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -58,34 +59,83 @@ dealer_cards << deck.pop
 dealer_total = calculate_total(dealer_cards)
 my_total = calculate_total(my_cards)
 
-#Show Cards
-#say "You're cards are" + my_cards.to_s
-#say "The dealers has" + dealer_cards.to_s
+#Show First Cards
 say "-----------------------------------------"
 say "-----------------------------------------"
-say "Dealer has: #{dealer_cards[0]} and #{dealer_cards[1]}"
-say "You have: #{my_cards[0]} and #{my_cards[1]}"
-say "Your total is #{my_total}, the dealer has #{dealer_total}"
+say "Dealer has: #{dealer_cards[0]} and *card hidden*"
+say "#{player_name.capitalize} has: #{my_cards[0]} and #{my_cards[1]}"
+say "#{player_name.capitalize}s total is #{my_total}."
 say "-----------------------------------------"
 say "-----------------------------------------"
 say ""
-say "What would you like to do? 1) hit 2) stay"
-hit_or_stay = gets.chomp
-say ""
-if hit_or_stay = '1'
-  say "You chose hit, good luck"
-  my_cards << deck.pop
-  say "-----------------------------------------"
-  say "-----------------------------------------"
-  say "Dealer has: #{dealer_cards[0]} and #{dealer_cards[1]}"
-  say "You have: #{my_cards[0]},#{my_cards[1]} and #{my_cards[2]}"
-  my_total = calculate_total(my_cards)
-  say "Your total is #{my_total}, the dealer has #{dealer_total}"
-  say "-----------------------------------------"
-  say "-----------------------------------------"
-  say ""
-  say "What would you like to do? 1) hit 2) stay"
+
+
+#Player Turn Iterator
+while my_total < 21
+
+  say "What would you like to do #{player_name.capitalize}? enter 1) for HIT or 2) for STAY"
   hit_or_stay = gets.chomp
-  say ""
-else 
+  
+#Hit or stay conditional
+  if hit_or_stay == '1'
+    my_cards << deck.pop
+    my_total = calculate_total(my_cards)
+    say "Dealer has: #{dealer_cards[0]} and *card hidden*"
+    say "#{player_name.capitalize} has: #{my_cards[0]} #{my_cards[1]} #{my_cards[2]} #{my_cards[3]} #{my_cards[4]}"
+    say "The totals are #{player_name.capitalize} - #{my_total}."
+  
+  elsif hit_or_stay == '2'
+    break
+  end
 end
+
+    #Blackjack edgecase conditional 
+  
+  if my_total == 21
+    say "Congratulations #{player_name.capitalize}, you got blackjack you win" 
+    exit
+  end
+
+  if my_total > 21
+    say "Sorry #{player_name.capitalize}, you're bust"
+    exit
+  end
+
+say "*the dealer turns over their card*"
+say "Dealer has: #{dealer_cards[0]} #{dealer_cards[1]}"
+say "#{player_name.capitalize}s total is #{my_total}, the dealer has #{dealer_total}" 
+
+#Dealer Turn Iterator
+while dealer_total < 17
+    say "The dealer hits"
+    dealer_cards << deck.pop
+    dealer_total = calculate_total(dealer_cards)
+    say "Dealer has: #{dealer_cards[0]} #{dealer_cards[1]} #{dealer_cards[2]} #{dealer_cards[3]} #{dealer_cards[4]}"
+    say "#{player_name.capitalize} has: #{my_cards[0]} #{my_cards[1]} #{my_cards[2]} #{my_cards[3]} #{my_cards[4]}"
+    say "#{player_name.capitalize}s total is #{my_total}, the dealer has #{dealer_total}"
+end
+    
+if dealer_total == 21
+  say "Sorry #{player_name.capitalize}, the dealer got blackjack you lose"
+  exit
+end
+
+if dealer_total > 21
+  say "The dealer is bust, you win!"
+  exit
+
+elsif dealer_total > my_total
+  puts "The dealer has #{dealer_total}, you have #{my_total}"
+  puts "Sorry #{player_name.capitalize}, the dealer wins"
+
+elsif dealer_total == my_total
+  puts "The dealer has #{dealer_total}, you have #{my_total}"
+  puts "It's a draw, no one wins"
+  exit
+
+elsif my_total > dealer_total
+  puts "Sweet, you have #{my_total}, the dealer has #{dealer_total}"
+  puts "#{player_name.capitalize} wins"
+  exit
+end
+  
--- blackjack_3.rb
+++ blackjack_3.rb
@@ -1,5 +1,4 @@
 #Interactive commandline blackjack game
-
 def say(msg)
   puts "#{msg}"
 end
@@ -18,7 +17,7 @@ def calculate_total(cards)
       total += value.to_i
     end
   end
-  
+
   arr.select{|e| e == 'A'}.count.times do
     total -= 10 if total > 21
   end
@@ -26,30 +25,40 @@ def calculate_total(cards)
   total
 end
 
+def play_again(my_cards, dealer_cards)
+  playing = true
+  my_cards.delete_if{ |e| e == true }
+  dealer_cards.delete_if{ |e| e == true }
 
-
-say "welcome to blackjack"
-say "What should I call you?"
-player_name = gets.chomp
-say "#{player_name.capitalize}, you have $200. How much would you like to bet?"
-bet = gets.chomp.to_i 
-while true
-  if bet > 0 
-    say "You've bet $#{bet}, lets play!"
-  break
-  else 
-    puts "Don't be stingy"
-    say "You have $200, how much would you like to bet?"
-    bet = gets.chomp.to_i
-  end
 end
 
-suits = ['H', 'D', 'S', 'C']
+#Start Game
+say "-----------------------------------------"
+say "-----------------------------------------"
+say "Welcome to Blackjack"
+say "-----------------------------------------"
+say "-----------------------------------------"
+say "What should I call you?"
+player_name = gets.chomp
+say "Hi #{player_name.capitalize}, lets play"
+
+# while true
+#   if bet > 0 
+#     say "You've bet $#{bet}, lets play!"
+#   break
+#   else 
+#     puts "Don't be stingy, the minimum bet is $1"
+#     say "You have $200, how much would you like to bet?"
+#     bet = gets.chomp.to_i
+#   end
+# end
+
+suits = ['H', 'D', 'S', 'C', 'H', 'D', 'S', 'C', 'H', 'D', 'S', 'C', 
+         'H', 'D', 'S', 'C', 'H', 'D', 'S', 'C', 'H', 'D', 'S', 'C',]
 cards = ['2', '3', '4', '5', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 deck = suits.product(cards)
 deck.shuffle!
-
 my_cards = []
 dealer_cards = []
 
@@ -61,39 +70,94 @@ dealer_cards << deck.pop
 dealer_total = calculate_total(dealer_cards)
 my_total = calculate_total(my_cards)
 
-#Show Cards
-#say "You're cards are" + my_cards.to_s
-#say "The dealers has" + dealer_cards.to_s
+playing = true
+while playing
+  say "-----------------------------------------"
+  say "-----------------------------------------"
+  say "Dealer has: #{dealer_cards[0]}, #{dealer_cards[1]} , #{dealer_cards[2]}, #{dealer_cards[3]}, #{dealer_cards[4]}"
+  say "You have: #{my_cards[0]}, #{my_cards[1]}, #{my_cards[2]}, #{my_cards[3]}, #{my_cards[4]}"
+  my_total = calculate_total(my_cards)
+  say "Your total is #{my_total}, the dealer has #{dealer_total}"
+  say "-----------------------------------------"
+  say "-----------------------------------------"
+  say "What would you like to do #{player_name.capitalize}? 1) HIT or 2) STAY"
+  hit_or_stay = gets.chomp
+  
+  if my_total == 21
+  say "You got Blackjack!"
+  say "Play again? 1) YES 2) NO"
+  play = gets.chomp
+    if play == '1'
+      my_cards.delete('H', 'S', 'D', 'C')
+      play_again
+    else 
+      puts "#{player_name.capitalize} leaves the table with. See you next time"
+      playing = false
+    end
+  end
 
-while my_total <= 21 || dealer_total <= 21
-say "-----------------------------------------"
-say "-----------------------------------------"
-say "Dealer has: #{dealer_cards[0]} and #{dealer_cards[1]}"
-say "You have: #{my_cards[0]}, #{my_cards[1]}, #{my_cards[2]}"
-say "Your total is #{my_total}, the dealer has #{dealer_total}"
-say "-----------------------------------------"
-say "-----------------------------------------"
-say ""
-say "What would you like to do? 1) hit 2) stay"
-hit_or_stay = gets.chomp
-say ""
+  if hit_or_stay == '1'
+    my_cards << deck.pop
+    my_total = calculate_total(my_cards)
+    
+    if my_total > 21
+      say "You're bust"
+      say "Play again? 1) YES 2) NO"
+      play = gets.chomp
+      
+      if play == '1'
+        play_again(my_cards, dealer_cards)
+      else 
+        puts "#{player_name.capitalize} leaves the table. See you next time"
+        playing = false
+      end
+    
+    end
+  
+  elsif hit_or_stay == '2'
+    
+    while dealer_total < 17
+      say "Dealer hits"
+      dealer_cards << deck.pop
+      dealer_total = calculate_total(dealer_cards)
+      say "Dealer total is #{dealer_total}"   
+    end           
+    
+    if dealer_total > my_total
+    say "Sorry #{player_name.capitalize}, the dealer wins :-("
+    say "Play again? 1) YES 2) NO"
+    play = gets.chomp
+      
+      if play == '1'
+        my_cards.delete('H', 'S', 'D', 'C')
+        play_again(my_cards, dealer_cards)
+      else 
+        puts "#{player_name.capitalize} leaves the table. See you next time"
+        playing = false
+      end  
+
+    elsif my_total > 21 
+    say "You're bust"
+    say "Play again? 1) YES 2) NO"
+    play = gets.chomp
+      if play == '1'
+        play_again(my_cards, dealer_cards)
+      else 
+        puts "#{player_name.capitalize} leaves the table with. See you next time"
+        playing = false
+      end
+    else
+    say "You win!"
+    say "Play again? 1) YES 2) NO"
+    play = gets.chomp
+    
+      if play == '1'
+        play_again(my_cards, dealer_cards)
+      else 
+        puts "#{player_name.capitalize} leaves the table. See you next time"
+        playing = false
+      end
+  
+    end
+  end
 end
-
-
-# if hit_or_stay = '1'
-#   say "You chose hit, good luck"
-#   my_cards << deck.pop
-#   say "-----------------------------------------"
-#   say "-----------------------------------------"
-#   say "Dealer has: #{dealer_cards[0]} and #{dealer_cards[1]}"
-#   say "You have: #{my_cards[0]},#{my_cards[1]} and #{my_cards[2]}"
-#   my_total = calculate_total(my_cards)
-#   say "Your total is #{my_total}, the dealer has #{dealer_total}"
-#   say "-----------------------------------------"
-#   say "-----------------------------------------"
-#   say ""
-#   say "What would you like to do? 1) hit 2) stay"
-#   hit_or_stay = gets.chomp
-#   say ""
-# else 
-# end
